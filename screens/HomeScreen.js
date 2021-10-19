6de574290d3d2231c6f54149e477005a6885c24e
Reeju Bhattacherji <reejubhattacherji@gmail.com>
2021-10-19 12:25:32 +0530

    fixing bug in bottom tab component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, SafeAreaView, StyleSheet, ScrollView } from "react-native";
+import BottomTabs, { bottomTabIcons } from "../component/home/BottomTabs";
 import Header from "../component/home/Header";
 import Post from "../component/home/Post";
 import Stories from "../component/home/Stories";
@@ -15,6 +16,7 @@ const HomeScreen = () => {
           <Post post={post} key={index} />
         ))}
       </ScrollView>
+      <BottomTabs icons={bottomTabIcons} />
     </SafeAreaView>
   );
 };
